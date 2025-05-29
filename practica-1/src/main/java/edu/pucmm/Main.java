f30f6210e156f1dc374c6dc830d9eba22c9713c2
MasterBruh <cdavid1962@hotmail.com>
2025-05-29 09:31:27 -0400

    prueba del primer push

--- practica-1/src/main/java/edu/pucmm/Main.java
+++ practica-1/src/main/java/edu/pucmm/Main.java
@@ -5,7 +5,7 @@ package edu.pucmm;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // to see how IntelliJ IDEA suggests fixaing it.
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {

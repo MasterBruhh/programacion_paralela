fa93c33d32e10b23cd45aa2b352fff1e4afed5a6
MasterBruh <cdavid1962@hotmail.com>
2025-05-29 18:11:07 -0400

    Suma secuencial (recibe el archivo generado)

--- practica-1/src/main/java/edu/pucmm/Main.java
+++ practica-1/src/main/java/edu/pucmm/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Generando datos...");
         Generador.generarArchivo("numeros.txt", 1_000_000);
+
+        System.out.println("\nEjecutando suma secuencial...");
+        SumaSecuencial.ejecutar("numeros.txt");
     }
 }
--- practica-1/src/main/java/edu/pucmm/SumaSecuencial.java
+++ practica-1/src/main/java/edu/pucmm/SumaSecuencial.java
@@ -1,4 +1,20 @@
 package edu.pucmm;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class SumaSecuencial {
+    public static void ejecutar(String archivo) throws IOException {
+        List<String> lineas = Files.readAllLines(Paths.get(archivo));
+        long inicio = System.nanoTime();
+        long suma = 0;
+        for (String linea : lineas) {
+            suma += Integer.parseInt(linea);
+        }
+        long fin = System.nanoTime();
+        System.out.println("Suma secuencial: " + suma);
+        System.out.println("Tiempo (s): " + (fin - inicio) / 1_000_000_000.0);
+    }
 }

4a1c507cf36eeb57bf04eb1d3cc1fc117b3be55d
Jean Carlos Perez <jcpo0002@ce.pucmm.edu.do>
2025-06-10 01:18:14 -0400

    Implementacion de StarNetwork

---
+++ practica-2/src/main/java/edu/pucmm/topology/StarNetwork.java
@@ -0,0 +1,70 @@
+package edu.pucmm.topology;
+
+import edu.pucmm.core.Message;
+import edu.pucmm.core.Node;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class StarNetwork implements NetworkTopology {
+
+    private final Map<Integer, Node> nodes = new HashMap<>();
+    private Node centralNode;
+    private ExecutorService executor;
+
+    @Override
+    public void configureNetwork(int numberOfNodes) {
+        // Crear los nodos
+        for (int i = 0; i < numberOfNodes; i++) {
+            nodes.put(i, new Node(i));
+        }
+
+        // Asignar el nodo central (por ejemplo, el nodo 0)
+        centralNode = nodes.get(0);
+
+        // Conectar cada nodo al nodo central (estrella)
+        for (int i = 1; i < numberOfNodes; i++) {
+            Node node = nodes.get(i);
+            node.addNeighbor(centralNode);
+            centralNode.addNeighbor(node);
+        }
+    }
+
+    @Override
+    public void sendMessage(int sourceId, int destinationId, String message) {
+        if (!nodes.containsKey(sourceId) || !nodes.containsKey(destinationId)) {
+            System.out.println("Nodo no válido.");
+            return;
+        }
+
+        // Enviar a través del nodo central
+        Node source = nodes.get(sourceId);
+        Node destination = nodes.get(destinationId);
+
+        if (sourceId != centralNode.getId()) {
+            centralNode.receiveMessage(new Message(sourceId, centralNode.getId(), "[Central Relay] -> " + message,System.currentTimeMillis()));
+        }
+
+        if (destinationId != centralNode.getId()) {
+            destination.receiveMessage(new Message(centralNode.getId(), destinationId, message,System.currentTimeMillis()));
+        }
+    }
+
+    @Override
+    public void startSimulation() {
+        executor = Executors.newFixedThreadPool(nodes.size());
+        for (Node node : nodes.values()) {
+            executor.submit(node);
+        }
+    }
+
+    @Override
+    public void printNetworkState() {
+        System.out.println("Topología en Estrella:");
+        System.out.println("Nodo central: " + centralNode.getId());
+        for (Node node : nodes.values()) {
+            System.out.println("Nodo " + node.getId() + " conectado a: " +
+                    node.getNeighborIds());
+        }
+    }
+}

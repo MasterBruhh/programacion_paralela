6b13c7d367ababadb75659327ccdb74afc4348e5
MasterBruhh <cdavid1962@hotmail.com>
2025-07-01 01:03:57 -0400

    arreglo nuevamente del build.gradle

--- primer_avance/build.gradle.kts
+++ primer_avance/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("application")
-    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "edu.pucmm"
@@ -18,22 +18,26 @@ java {
 }
 
 dependencies {
-    implementation("org.openjfx:javafx-controls:21")
-    implementation("org.openjfx:javafx-fxml:21")
-    implementation("org.openjfx:javafx-graphics:21")
+    implementation("org.openjfx:javafx-controls:22")
+    implementation("org.openjfx:javafx-fxml:22")
+    implementation("org.openjfx:javafx-graphics:22")
+    
+    // logging
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
 }
 
 javafx {
-    version = "21"
+    version = "22"
     modules = listOf("javafx.controls", "javafx.fxml", "javafx.graphics")
 }
 
 application {
     // Cambia esta clase por tu clase principal real
-    mainClass.set("com.trafico.Main")
+    mainClass.set("edu.pucmm.Main")
 }
 
 tasks.test {
